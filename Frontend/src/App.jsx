076e5fce939f9c48b201b7f84bf770182c64a2e2
murarii07 <abhayaj07@gmail.com>
2024-09-15 22:22:12 +0530

    f

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,16 +9,17 @@ function App() {
   const handleImageChange = (event) => {
     const file = event.target.files[0];
     setImage(URL.createObjectURL(file));
+    console.log(image)
   }
 
   const handleImageUpload = async () => {
     //api call 
-
     const formData = new FormData();
     formData.append("file", image);
-    const res=await fetch("http://localhost:8000/",{method:'POST',body:formData})
+    
+    const res=await fetch("http://192.168.43.157:8000",{method:'POST', mode: 'cors', body:formData})
     if(res.ok){
-      const blob=await response.blob();
+      const blob=await res.blob();
       const e=URL.createObjectURL(blob);
       setColorizedImage(e);
     }
@@ -28,12 +29,12 @@ function App() {
     }
 
   }
-
   return (
+    
     <div className="bg-black h-full text-white bg-gradient-to-b from-black to-gray-900">
       <div className="text-2xl p-4 font-bold text-blue-500 shadow-2xl bg-black">SAR IMAGE COLORIZARION</div>
       <div className="flex flex-col items-center gap-2 p-20">
-      <label className="font-semibold text-xl" for="file">
+      <label className="font-semibold text-xl"  htmlFor="file">
         Select Image
       </label>
       <input className="pl-20" type="file" id="file" onChange={handleImageChange}></input>
